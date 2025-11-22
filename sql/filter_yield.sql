673857ebcf778a8b583e9e237a3db6df0957a0bd
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-22 15:18:42 -0800

    Update filter_yield.sql

--- sql/filter_yield.sql
+++ sql/filter_yield.sql
@@ -21,3 +21,28 @@ ORDER BY q.item ASC
 
 
 -- Alternatively, one can use a HAVING COUNT(DISTINCT country) = 3 instead of the method used in cte, but this method obtains complete data.
+-- Following is the alternative mentioned:
+/*
+WITH cte AS(
+SELECT Item FROM prices
+	WHERE Unit = 'SLC'
+    AND Months = 'Annual Value'
+GROUP BY Item
+HAVING COUNT(DISTINCT Area) = 3
+-- WHERE Unit NOT IN("")
+)
+SELECT 
+		p.Domain,
+		p.Area, 
+		p.Element,
+		p.Item,
+		p.Year,
+		p.Months,
+		p.Unit, 
+		p.Value
+ FROM prices p
+	JOIN cte c
+		ON p.Item = c.Item
+	WHERE Unit = 'SLC'
+    AND Months = 'Annual Value'
+*/
