cae4c23e2ed4e400044065f041ff97bb813bf593
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-22 18:41:43 -0800

    Update Scatterplots.py

--- python/Scatterplots.py
+++ python/Scatterplots.py
@@ -1,33 +1,54 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv('first_scatter.csv')
-df2 = pd.read_csv('second_scatter.csv')
+df = pd.read_csv('/Users/d404me/Downloads/first_scatter.csv')
+df2 = pd.read_csv('/Users/d404me/Downloads/second_scatter.csv')
+df3 = pd.read_csv('/Users/d404me/Downloads/third_scatter.csv')
 
 
-y = df['emissions_avg']
-y2 = df2['avg_yields']
+y = 'emissions_avg'
+y2 = 'avg_yields'
 
-x = df['temperatures_avg']
+x = 'temperatures_avg'
+
+colors = {'Canada': 'red', 'Mexico': 'mediumaquamarine', 'United States of America': 'dodgerblue'}
 
 
 plt.figure(figsize=(8,6))
 
-plt.scatter(x,y)
+for Country, group in df.groupby('Country'):
+    plt.scatter(group[x],group[y], color = colors[Country], label = Country)
+
+
+#plt.scatter(x,y)
 plt.ylabel("Average Emissions (kt)")
-plt.xlabel("Average Temperatures (\u00b0 C)")
+plt.xlabel("Average Temperature Change (\u00b0 C)")
 plt.title("Average Total Emissions vs Average Changes in Temperature", loc = 'center')
+plt.legend()
+plt.grid(True)
+
 
 plt.figure(figsize=(8,6))
 
-plt.scatter(x,y2)
+for Country, group in df2.groupby('Country'):
+    plt.scatter(group[x],group[y2], color = colors[Country], label = Country)
+
+#plt.scatter(x,y2)
 plt.ylabel("Average Yields (kg/ha)")
-plt.xlabel("Average Temperatures (\u00b0 C)")
+plt.xlabel("Average Temperature Change (\u00b0 C)")
 plt.title("Average Yields vs Average Changes in Temperature", loc = 'center')
+plt.legend()
+plt.grid(True)
 
 plt.figure(figsize=(8,6))
 
-plt.scatter(y,y2)
+for Country, group in df3.groupby('Country'):
+    plt.scatter(group[y],group[y2], color = colors[Country], label = Country)
+
+
+#plt.scatter(y,y2)
 plt.ylabel("Average Yields (kg/ha)")
 plt.xlabel("Average Total Emissions (kt)")
 plt.title("Average Yields vs Average Total Emissions", loc = 'center')
+plt.legend()
+plt.grid(True)
